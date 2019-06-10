2c8395d70bb57fd8c58bda48fb6fde18aad5c898
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-06-10 17:34:48 -0500

    prompt 2 messages

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,29 +11,42 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err){
-    if (err) throw err;
+    if (err) 
+    // throw err;
     console.log("\nWelcome to Bamazon!" );
     allProducts();
 });
 
 function allProducts() {
-    connection.query("SELECT * from products;", function(err, results, fields) {
-        if (err) throw err;
+    connection.query("SELECT * FROM products;", function(err, results, fields) {
+        if (err); 
+        // throw err;
         else {
         console.table(results);
         }
       pickProduct();
-     
     }
-    
-)}
-
+)};
 
 function pickProduct() {
     inquirer.prompt([
         {
-            name: "Product",
-            message: 
+            name: "Id",
+            type: "number",
+            message: "What is the item id you would like to purchase?"
+
+        },
+        {
+            name: "quantity",
+            type: "nnumber",
+            message: "How many units of the product would you like purchase?"
         }
-    ])
+    ]).then(function(answer){
+        var product = answer.Id;
+        var quantity = answer.quantity;
+        
+        connection.query("SELECT * FROM products WHERE id= ")
+
+
+    })
 }
